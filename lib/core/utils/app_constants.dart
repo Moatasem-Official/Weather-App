3f54da328455ea30e7f79b769a50248595a433a3
Moatasem <moatasemnagy3@gmail.com>
2025-08-15 21:34:21 +0300

    Apply Core folder To App

---
+++ lib/core/utils/app_constants.dart
@@ -0,0 +1,3 @@
+const String kWeatherApiKey = '005280c3705048a8aef181748251206';
+const String kWeatherApiBaseUrl = 'https://api.weatherapi.com/v1/';
+const String kWeatherApiEndpoint = 'forecast.json';
